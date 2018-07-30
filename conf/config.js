96a4acba660637ab60b66ed350d709af83a0a438
unknown <501981226@qq.com>
2018-07-30 17:49:25 +0800

    20180730

--- conf/config.js
+++ conf/config.js
@@ -6,7 +6,7 @@
 //     photosServer:'http://127.0.0.1:3002'
 // };
 
-exports.serverName='http://127.0.0.1:3006';
+exports.serverName='http://116.62.173.184:3006';
 
 
-exports.photosServer='http://127.0.0.1:3002';
+exports.photosServer='http://116.62.173.184:3002';
